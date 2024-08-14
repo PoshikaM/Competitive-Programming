89b1710fa8c17b239058db066f55d18dd04b7c12
Poshika <poshika@Poshikas-MacBook-Air.local>
2024-08-14 21:43:58 +0530

    Day 5

--- Day5/InsertionSort.py
+++ Day5/InsertionSort.py
@@ -1,12 +1,11 @@
 n = int(input())
 arr = list(map(int, input().split()))
 for i in range(1, n):
-    curr = arr[i]
+    currEle = arr[i]
     j = i-1
 
-    while j >= 0 and curr < arr[j]:
+    while j >= 0 and currEle < arr[j]:
         arr[j+1] = arr[j]
         j -= 1 
-
-    arr[j+1] = curr
-print(*arr)
+    arr[j+1] = currEle
+print(*arr) 
